e43004c9fa2aaa996c8f7450398c488e1bdcfaeb
cp-131577 <cp-131577@hs-weingarten.de>
2015-07-02 18:57:36 +0200

    Aufg 20 - Mocks fertig

--- biblio-service/src/test/java/de/hrw/swep/biblio/service/BibliothekTestMitMock.java
+++ biblio-service/src/test/java/de/hrw/swep/biblio/service/BibliothekTestMitMock.java
@@ -1,8 +1,21 @@
 package de.hrw.swep.biblio.service;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.*;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.AdditionalMatchers.*;
+import de.hrw.swep.biblio.persistence.DBInterface;
+import de.hrw.swep.biblio.persistence.dto.BenutzerDTO;
+import de.hrw.swep.biblio.persistence.dto.BuchDTO;
+import de.hrw.swep.biblio.service.benutzer.Benutzer;
+import de.hrw.swep.biblio.service.gegenstaende.Buch;
 
 /**
  * Testet die Bibliotheks-Klasse mit einem Mock-Objekt
@@ -10,13 +23,44 @@ import org.junit.Test;
  *
  */
 public class BibliothekTestMitMock {
+	
+	Bibliothek bib;
+	
+	/**
+	 * Mocks
+	 */
+	@Before
+	public void setUp() {
+		DBInterface dbi = Mockito.mock(DBInterface.class);
+		
+		BenutzerDTO b = new BenutzerDTO(1, "Chris Pfeffi", "Normal");
+		Set<BenutzerDTO> bSet = new HashSet<BenutzerDTO>();
+		bSet.add(b);
+		
+		BuchDTO buch = new BuchDTO(1, "Stinson", "Playbook", "Frei");
+		Set<BuchDTO> buchSet = new HashSet<BuchDTO>();
+		buchSet.add(buch);
+		
+		when(dbi.getBenutzerById(1)).thenReturn(b);
+		when(dbi.getBenutzerByName("Chris Pfeffi")).thenReturn(bSet);
+		when(dbi.getBuchByAutor("Stinson")).thenReturn(buchSet);
+		when(dbi.getBuchByTitle("Playbook")).thenReturn(buchSet);
+		
+		
+		
+		bib = new Bibliothek();
+		bib.setDb(dbi);
+		
+	}
 
 	/**
 	 * Testet, ob ein Buch mit gegebenem Titel gefunden wird.
 	 */
 	@Test
 	public void testSucheBuchNachTitel() {
-		
+		Set<Buch> b = bib.sucheBuchNachTitel("Playbook");
+		assertEquals("Stinson", b.iterator().next().getAutor());
+		assertEquals(1, b.size());
 	}
 
 	/**
@@ -24,7 +68,9 @@ public class BibliothekTestMitMock {
 	 */
 	@Test
 	public void testSucheBuchNachAutor() {
-		fail("Not yet implemented");
+		Set<Buch> b = bib.sucheBuchNachAutor("Stinson");
+		assertEquals(1, b.size());
+		assertEquals("Playbook", b.iterator().next().getTitel());
 	}
 
 	/**
@@ -32,7 +78,9 @@ public class BibliothekTestMitMock {
 	 */
 	@Test
 	public void testSucheBenutzerNachName() {
-		fail("Not yet implemented");
+		Set<Benutzer> bSet = bib.sucheBenutzerNachName("Chris Pfeffi");
+		assertEquals(1, bSet.size());
+		assertEquals(1, bSet.iterator().next().getId());
 	}
 
 	/**
@@ -40,7 +88,10 @@ public class BibliothekTestMitMock {
 	 */
 	@Test
 	public void testSucheBenutzerNachId() {
-		fail("Not yet implemented");
+		Benutzer b = bib.sucheBenutzerNachId(1);
+		assertEquals("Chris Pfeffi", b.getName());
+		assertEquals("de.hrw.swep.biblio.service.benutzer.Normal", b.getStatus().getClass().getName());
+		
 	}
 
 }
